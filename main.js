751eabe0301af4270c6cc78e964992abbac4d201
ben awad <benawad97@gmail.com>
2016-02-28 16:13:23 -0600

    Unstyled population vs county bar graph

--- main.js
+++ main.js
@@ -1,69 +1,144 @@
-$(document).ready(function() {
-   var title = {
-      text: 'Monthly Average Temperature'   
-   };
-   var subtitle = {
-      text: 'Source: WorldClimate.com'
-   };
-   var xAxis = {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-   };
-   var yAxis = {
-      title: {
-         text: 'Temperature (\xB0C)'
-      },
-      plotLines: [{
-         value: 0,
-         width: 1,
-         color: '#808080'
-      }]
-   };   
+// read csv
+$.ajax({
+	url: "https://raw.githubusercontent.com/benawad/tex-protects/gh-pages/data.csv?token=AHgfSfaI3i_WoVkZC-_w2bfuTPW6sRiZks5W3KLpwA%3D%3D",
+    async: true,
+    success: function (csvd) {
+				// convert csv to objects
+        data = $.csv.toObjects(csvd);
+    },
+    dataType: "text",
+    complete: function () {
+			// get first five entries
+			var sample = data.slice(0, 5);	
+			// make bar graph
+			barGraph(sample);
+    }
+});
 
-   var tooltip = {
-      valueSuffix: '\xB0C'
-   }
+function barGraph(data){
 
-   var legend = {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'middle',
-      borderWidth: 0
-   };
+	var chart = {
+		type: 'bar'
+	};
 
-   var series =  [
-      {
-         name: 'Tokyo',
-         data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,
-            26.5, 23.3, 18.3, 13.9, 9.6]
-      }, 
-      {
-         name: 'New York',
-         data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,
-            24.1, 20.1, 14.1, 8.6, 2.5]
-      }, 
-      {
-         name: 'Berlin',
-         data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6,
-            17.9, 14.3, 9.0, 3.9, 1.0]
-      }, 
-      {
-         name: 'London',
-         data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 
-            16.6, 14.2, 10.3, 6.6, 4.8]
-      }
-   ];
+	var title = {
+		text: 'Child Population vs. Confirmed Victims of Child Abuse/Neglect'
+	};
+
+	var counties = [];
+	for(var i = 0; i < 5; i++){
+		counties.push(data[i].County);
+	}
+
+	var xAxis = {
+		title:{
+			text: "Counties"
+		},
+		categories: counties
+	};
+	var yAxis = {
+		title:{
+			text: "Population"
+		},
+		plotLines: [{
+			 value: 0,
+			 width: 1,
+			 color: '#808080'
+		}]
+	};
+
+	var populations = [];
+	
+	for(var i = 0; i < 5; i++){
+		populations.push(+data[i]["Child Population"]);
+	}
+
+	var series = [{
+		data:populations				
+	}];
+
+	console.log(populations);
 
    var json = {};
 
+	 json.chart = chart;
    json.title = title;
-   json.subtitle = subtitle;
    json.xAxis = xAxis;
    json.yAxis = yAxis;
-   json.tooltip = tooltip;
-   json.legend = legend;
    json.series = series;
 
    $('#container').highcharts(json);
-});
 
+}
+
+//function bob (data) {
+	//var title = {
+		//text: 'Average Temperatures of Cities'   
+	//};
+	//var subtitle = {
+		//text: 'Source: worldClimate.com'
+	//};
+
+	//var counties = [];
+	//for(var i = 0; i < 5; i++){
+		//counties.push(data[i].County);
+	//}
+
+	//var xAxis = {
+		//categories: counties
+	//};
+	//var yAxis = {
+		//title: {
+			 //text: 'Temperature (\xB0C)'
+		//},
+		//plotLines: [{
+			 //value: 0,
+			 //width: 1,
+			 //color: '#808080'
+		//}]
+	//};   
+
+	//var tooltip = {
+		//valueSuffix: '\xB0C'
+	//}
+
+	//var legend = {
+		//layout: 'vertical',
+		//align: 'right',
+		//verticalAlign: 'middle',
+		//borderWidth: 0
+	//};
+
+	//var populations = [];
+	
+	//for(var i = 0; i < 5; i++){
+		//populations.push(+data[i]["Child Population"]);
+	//}
+
+	//var series =  [
+		//{
+			 //name: 'Tokyo',
+			 //data: [7.0, 6.9, 9.5, 14.5, 18.2]
+		//}, 
+		//{
+			 //name: 'New York',
+			 //data: [-0.2, 0.8, 5.7, 11.3, 17.0]
+		//},
+		//{
+			 //name: 'London',
+			 //data: [3.9, 4.2, 5.7, 8.5, 11.9]
+		//}
+	//];
+
+	//var json = {};
+
+	//json.title = title;
+	//json.subtitle = subtitle;
+	//json.xAxis = xAxis;
+	//json.yAxis = yAxis;
+	//json.tooltip = tooltip;
+	//json.legend = legend;
+	//json.series = series;
+
+	//$('#container').highcharts(json);
+//}
